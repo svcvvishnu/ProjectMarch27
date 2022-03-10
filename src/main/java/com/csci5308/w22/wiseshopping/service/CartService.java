4c50e4f41476c07cb8e0b58509c63cdee4e232e4
Adarsh Iyengar <ad398244@dal.ca>
2022-03-10 17:31:07 -0400

    issue#18 Cart Sharing

---
+++ src/main/java/com/csci5308/w22/wiseshopping/service/CartService.java
@@ -0,0 +1,49 @@
+package com.csci5308.w22.wiseshopping.service;
+
+import com.csci5308.w22.wiseshopping.exceptions.NoCartCreatedForUserException;
+import com.csci5308.w22.wiseshopping.models.Cart;
+import com.csci5308.w22.wiseshopping.models.SharedCart;
+import com.csci5308.w22.wiseshopping.models.User;
+import com.csci5308.w22.wiseshopping.repository.CartRepository;
+import com.csci5308.w22.wiseshopping.repository.SharedCartRepository;
+import com.csci5308.w22.wiseshopping.repository.UserRepository;
+import com.csci5308.w22.wiseshopping.utils.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author Adarsh Kannan
+ */
+@Service
+public class CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private SharedCartRepository sharedCartRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public Cart shareCart( User currentUser, List<User> otherUsersToShare){
+        Cart cart = cartRepository.findByUser(currentUser);
+        if (cart == null){
+            throw new NoCartCreatedForUserException("no cart for user " + currentUser);
+        }
+        cart.setStatus(Constants.SHARED);
+        cartRepository.save(cart);
+        for (User user: otherUsersToShare){
+            SharedCart sharedCart = sharedCartRepository.findByUserAndCart(user,cart);
+            if (sharedCart == null) {
+                sharedCart = new SharedCart(user, cart);
+                sharedCartRepository.save(sharedCart);
+            }
+        }
+        return cart;
+    }
+}
